54a34572d1549883603f1a6bea6156838cd4b947
Zakaria <zknabdi@gmail.com>
2019-09-29 15:37:00 -0500

    Rearrange it to final

---
+++ app/src/main/java/com/training/popularmoviefinalapp/activity/MovieActivity.java
@@ -0,0 +1,15 @@
+package com.training.popularmoviefinalapp.activity;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+import com.training.popularmoviefinalapp.R;
+
+public class MovieActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_movie);
+    }
+}
